bc2957c20fe61a9497761e57d28db29798afa10f
dips3095 <dips3095@gmail.com>
2017-03-12 22:58:42 +0300

    Added Image and CSS optimization, autoprefix

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
 var gulp      = require('gulp'), // Подключаем Gulp
-    less        =require('gulp-less'),
-    browserSync = require('browser-sync'); // Подключаем Browser Sync
-
+    less        =require('gulp-less'),//Подключаем пакет для Less
+    cssnano     = require('gulp-cssnano'), // Подключаем пакет для минификации CSS
+    rename      = require('gulp-rename'), //Подключаем пакет для переименования
+    del         = require('del'),// Подключаем библиотеку для переименования файлов
+    browserSync = require('browser-sync'), //Подключаем browser-sync
+    imagemin    = require('gulp-imagemin'), // Подключаем библиотеку для работы с изображениями
+    pngquant    = require('imagemin-pngquant'), // Подключаем библиотеку для работы с png
+    cache       = require('gulp-cache'), // Подключаем библиотеку кеширования
+    autoprefixer = require('gulp-autoprefixer');// Подключаем библиотеку для автоматического добавления префиксов
 
 gulp.task('browser-sync', function() { // Создаем таск browser-sync
     browserSync({ // Выполняем browser Sync
@@ -14,13 +20,53 @@ gulp.task('browser-sync', function() { // Создаем таск browser-sync
 gulp.task('less', function(){ // Создаем таск Sass
     return gulp.src('app/less/**/*.less') // Берем источник
         .pipe(less()) // Преобразуем Less в CSS посредством gulp-sass
+        .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) // Создаем префиксы
         .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
         .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
 });
-gulp.task('watch', ['browser-sync','less'], function() {
+gulp.task('css-libs', ['less'], function() {
+    return gulp.src('app/css/style.css') // Выбираем файл для минификации
+        .pipe(cssnano()) // Сжимаем
+        .pipe(rename({suffix: '.min'})) // Добавляем суффикс .min
+        .pipe(gulp.dest('app/css')); // Выгружаем в папку app/css
+});
+gulp.task('watch', ['browser-sync','css-libs','less'], function() {
     gulp.watch('app/less/**/*.less',['less'],browserSync.reload);//    Наблюдение за less файлами в папке sass
     gulp.watch('app/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
     gulp.watch('app/js/**/*.js', browserSync.reload); // Наблюдение за JS файлами в папке js
 });
+gulp.task('clean', function() {
+    return del.sync('dist'); // Удаляем папку dist перед сборкой
+});
+gulp.task('build', ['clean','img','less'], function() {
 
+    var buildCss = gulp.src([ // Переносим CSS стили в продакшен
+        'app/css/style.css',
+        'app/css/style.min.css'
+    ])
+        .pipe(gulp.dest('dist/css'))
+
+    var buildFonts = gulp.src('app/fonts/**/*') // Переносим шрифты в продакшен
+        .pipe(gulp.dest('dist/fonts'))
+
+    var buildJs = gulp.src('app/js/**/*') // Переносим скрипты в продакшен
+        .pipe(gulp.dest('dist/js'))
+
+    var buildHtml = gulp.src('app/*.html') // Переносим HTML в продакшен
+        .pipe(gulp.dest('dist'));
+
+});
+gulp.task('img', function() {
+    return gulp.src('app/img/**/*') // Берем все изображения из app
+        .pipe(cache(imagemin({  // Сжимаем их с наилучшими настройками с учетом кеширования
+            interlaced: true,
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        })))
+        .pipe(gulp.dest('dist/img')); // Выгружаем на продакшен
+});
+gulp.task('clear', function () {
+    return cache.clearAll();
+})
 gulp.task('default', ['watch']);
