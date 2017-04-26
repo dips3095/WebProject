a3bb3e8371f284aab4a130898f19c67bcbd2014e
dips3095 <dips3095@gmail.com>
2017-04-27 00:45:38 +0300

    fixes

--- app/js/burger.js
+++ app/js/burger.js
@@ -1,4 +1,5 @@
 let burger = document.getElementById("burger-button");
+let area = document.getElementById("area");
 
 burger.addEventListener("click", (e) => {
     e.preventDefault();
--- app/js/readyjs/burger.js
+++ app/js/readyjs/burger.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var burger = document.getElementById("burger-button");
+var area = document.getElementById("area");
 
 burger.addEventListener("click", function (e) {
     e.preventDefault();
