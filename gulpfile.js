cd96d3f83d1090bf53c272e8d3125c7a1d7a1ac4
dips3095 <dips3095@gmail.com>
2017-05-12 11:52:51 +0300

    fix eslint

--- gulpfile.js
+++ gulpfile.js
@@ -58,7 +58,7 @@ gulp.task('lint', () => {
     // So, it's best to have gulp ignore the directory as well.
     // Also, Be sure to return the stream from the task;
     // Otherwise, the task may end before the stream has finished.
-    return gulp.src('app/js/*.js')
+    return gulp.src('app/js/es2015/*.js')
     // eslint() attaches the lint output to the "eslint" property
     // of the file object so it can be used by other modules.
         .pipe(eslint())
@@ -142,4 +142,4 @@ gulp.task('img', function() {
 gulp.task('clear', function () {
     return cache.clearAll();
 })
-gulp.task('default', ['watch']);
+gulp.task('default', ['watch', 'lint']);
